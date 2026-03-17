a58ceddbfa0de7f87cdbcb4289984721a60284c2
Odododueshuk <xuph9ma@gmail.com>
2026-03-17 15:00:39 +0800

    加入專案檔案。

---
+++ Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public class Test
+{
+	public int Mid;
+	public int Final;
+	public void SetFinal(int value)
+	{
+		Final = value;
+	}
+	public int GetFinal()
+	{
+		return Final;
+    }
+	public double GetAvg()
+	{
+		return (Mid + Final) / 2.0;
+    }
+	public void SetGrade(int m,int f)
+	{
+		if (VAildGrade(m, f))
+		{
+			Mid = m;
+			Final = f;
+		}
+	}
+	private bool VAildGrade(int m ,int f)
+	{
+
+		if (m >= 0 && m <= 100)
+		{
+			if(f >= 0 && f <= 100)
+			{
+				return true;
+			}
+		}
+		return false;
+			
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _20260317_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
